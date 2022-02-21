d0868092d0ec697dde5e547ac576f62fef1641e0
Romain Lamothe <romain@motla.fr>
2022-02-21 16:24:05 +0100

    bugfix: accept x86_64 architecture

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
 const os = require('os');
 
 // Set add-on object name depending on the current system processor architecture
-const node_library_file = `./build/Release/ftdi-d2xx.${os.type()}.${os.arch()}.node`;
-
-// Try to export the correct add-on object if it exists, else throw an error
+// and try to export the correct add-on object if it exists, else throw an error
 try {
-  module.exports = require(node_library_file);
+  module.exports = require(`./build/Release/ftdi-d2xx.${os.type()}.${os.arch()}.node`);
 } catch (e) {
-  throw(new Error(`ftdi-d2xx: The library has not been compiled for this architecture (${os.type()}/${os.arch()}). Run "cmake-js rebuild".`));
+  try {
+    if(os.arch() == "x86" || os.arch() == "x64") {
+      module.exports = require(`./build/Release/ftdi-d2xx.${os.type()}.x86_64.node`);
+    } else throw(true);
+  } catch (e) {
+    throw(new Error(`ftdi-d2xx: The library has not been compiled for this architecture (${os.type()}/${os.arch()}). Run "cmake-js rebuild".`));
+  }
 }
