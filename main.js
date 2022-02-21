572c316e2efd1d014114f2dd2961b6aa71282179
Romain Lamothe <romain@motla.fr>
2022-02-21 16:44:56 +0100

    bugfix: redirect Windows_NT and AMD64

--- main.js
+++ main.js
@@ -2,12 +2,19 @@ const os = require('os');
 
 // Set add-on object name depending on the current system processor architecture
 // and try to export the correct add-on object if it exists, else throw an error
+
+let os_type = os.type();
+if(os_type == "Windows_NT") os_type = "Windows";
+
+let os_arch = os.arch();
+if(os_type == "Windows" && os_arch == "x64") os_arch = "AMD64";
+
 try {
-  module.exports = require(`./build/Release/ftdi-d2xx.${os.type()}.${os.arch()}.node`);
+  module.exports = require(`./build/Release/ftdi-d2xx.${os_type}.${os_arch}.node`);
 } catch (e) {
   try {
     if(os.arch() == "x86" || os.arch() == "x64") {
-      module.exports = require(`./build/Release/ftdi-d2xx.${os.type()}.x86_64.node`);
+      module.exports = require(`./build/Release/ftdi-d2xx.${os_type}.x86_64.node`);
     } else throw(true);
   } catch (e) {
     throw(new Error(`ftdi-d2xx: The library has not been compiled for this architecture (${os.type()}/${os.arch()}). Run "cmake-js rebuild".`));
