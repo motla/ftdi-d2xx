67cedf53d9b387be83130d31da61a82d3f87a39c
Romain Lamothe <romain@motla.fr>
2022-03-16 09:51:47 +0100

    minor corrections

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
+//TODO check node version
+
 const os = require('os');
 
 // Set add-on object name depending on the current system processor architecture
-// and try to export the correct add-on object if it exists, else throw an error
+// and try to require it. It will throw an error if the file doesn't exist.
 
 let os_type = os.type();
 if(os_type == "Windows_NT") os_type = "Windows";
@@ -12,11 +14,14 @@ if(os_type == "Windows" && os_arch == "x64") os_arch = "AMD64";
 try {
   module.exports = require(`./build/Release/ftdi-d2xx.${os_type}.${os_arch}.node`);
 } catch (e) {
+
+  //TODO manage errors other than file not found
+  
   try {
     if(os.arch() == "x86" || os.arch() == "x64") {
       module.exports = require(`./build/Release/ftdi-d2xx.${os_type}.x86_64.node`);
     } else throw(true);
   } catch (e) {
-    throw(new Error(`ftdi-d2xx: The library has not been compiled for this architecture (${os_type}/${os_arch}). Run "cmake-js rebuild".`));
+    throw(new Error(`ftdi-d2xx: Library executable was not found for this architecture (${os_type}/${os_arch}). Run "cmake-js rebuild" from the ftdi-d2xx folder.`));
   }
 }
